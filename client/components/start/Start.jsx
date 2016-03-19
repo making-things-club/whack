9f38d03584a8356a5d1570a3d9a6b2c48ff52d09
Pete Goodman <thegingerbloke@users.noreply.github.com>
2016-03-19 16:04:23 +0000

    Add start page input field for game name creation

--- client/components/start/Start.jsx
+++ client/components/start/Start.jsx
@@ -1,6 +1,34 @@
+const { browserHistory } = ReactRouter;
+
 export default class Start extends React.Component {
 
+  constructor() {
+    super();
+    this.state = { value: ''}
+  }
+
+  goToGame() {
+    const gameName = this.state.gameName.replace(/[^a-zA-Z0-9-_]/g, '-');
+    browserHistory.push(`/g/${gameName}`);
+  }
+
+  setGameName(e) {
+    this.setState({gameName:e.target.value});
+  }
+
   render() {
-    return <div><p>Start</p></div>;
+    return (
+        <div>
+          <form onSubmit={()=>this.goToGame()}>
+            <label>Give your game a name</label>
+            <input
+              type="text"
+              value={this.state.gameName}
+              onChange={(e)=>this.setGameName(e)}
+            />
+            <button type="submit">Go</button>
+          </form>
+        </div>
+    );
   }
 }
