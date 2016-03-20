5bdb51ba83f480412c851dc1a1eb8d3f48331f83
Remi <remi.shergold@gmail.com>
2016-03-20 12:52:10 +0000

    list players waiting to start a game

--- client/components/ready/Ready.jsx
+++ client/components/ready/Ready.jsx
@@ -6,12 +6,26 @@ export default class Ready extends React.Component {
     this.props.startRound();
   }
 
+  renderPlayers() {
+    const {player, players} = this.props
+
+    if (players === null) return null
+
+    return players.map((thisPlayer)=>{
+      return (thisPlayer._id === player._id) ? <li><em>{thisPlayer.name}</em></li> : <li>{thisPlayer.name}</li>
+    })
+  }
+
   render() {
+    console.log(this.props)
+    const joinPath = `http://${window.location.hostname}:${window.location.port}/join/${this.props.roomId}`
     return(
       <div>
         <h1>Place your phone down on a table.</h1>
-        <p>Share this URL to join the game.</p>
+        <p>Share this URL to join the game. </p>
+        <a href={joinPath}>{joinPath}</a>
         <p><em>{this.props.params.roomId}</em></p>
+        <ul>{this.renderPlayers()}</ul>
         <p>Waiting for all the players to join before starting the game</p>
         <button onClick={()=>this.goToGame()}>Start game</button>
       </div>
