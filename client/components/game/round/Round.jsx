20395bd858157b2efc18e528db7efc6dc629b8c4
Iain <iainmbean@gmail.com>
2016-04-24 15:27:54 +0100

    Add vibrate on ingredient click

--- client/components/game/round/Round.jsx
+++ client/components/game/round/Round.jsx
@@ -4,8 +4,16 @@ import styles from './round.mss';
 
 export default class Round extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      supportsVibration : ('vibrate' in navigator)
+    }
+  }
+
   renderIngredient() {
-    if(this.props.player._id === this.props.room.pickedMoleId) {
+    if (this.props.player._id === this.props.room.pickedMoleId) {
       return <Ingredient ingredientClick={this.onIngredientClick.bind(this)} />;
     } else {
       return '';
@@ -15,6 +23,9 @@ export default class Round extends React.Component {
   onIngredientClick() {
     console.log('Mole clicked!');
     this.props.sampler.triggerAttack('samples.onion');
+    if (this.state.supportsVibration) {
+      window.navigator.vibrate(100)
+    }
     Meteor.call('whackMole', this.props.room._id, this.props.room.pickedMoleId, (error, result) => {
         console.log(error, result);
     });
